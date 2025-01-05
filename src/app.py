03f9e327599539121698c09d3f323f8761d17d1c
rabbledoo <scott.tarnowsky@gmail.com>
2025-01-05 09:11:58 -0500

    updated metadata

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
-app = FastAPI()
+app = FastAPI(
+    title="RAG API",
+    description="API for RAG. Connected to Google Analytics",
+    version="0,1"
+)
 
 @app.post("/chat", description="Chat with the RAG API through this endpoint")
 def chat(message: str):
